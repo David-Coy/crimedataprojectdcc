716d2a442a5e3bb94cf7186f97310f3889bd3fb2
David-Coy <davidcoy@protonmail.com>
2021-03-02 13:52:22 -0500

    another update

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ def Prediction():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
-    #app.run(debug=True)
+    #app.run(host='0.0.0.0')
+    app.run(debug=True)
     
     
 # if __name__ == '__main__':
