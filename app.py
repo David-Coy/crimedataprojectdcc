4a7f80ed89c0debcb7ca893afc8d40ffc527a911
David-Coy <davidcoy@protonmail.com>
2021-03-02 13:40:41 -0500

    another update

--- app.py
+++ app.py
@@ -85,8 +85,9 @@ def Prediction():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
-    #app.run(debug=True)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0')
+    
 # if __name__ == '__main__':
 #     HOST = environ.get('SERVER_HOST', 'localhost')
 #     print(HOST)
